b02a1e6647de80527c8f1cd52c47b614c192865f
DarrenM07 <darrenms7120@gmail.com>
2024-09-06 19:14:56 +0700

    ECommerce

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+    context = {
+        'name': 'Darren Marcello Sidabutar',
+        'class': 'PBP KKI',
+        'name_app': 'E-Commerce'
+    }
+
+    return render(request, "main.html", context)
